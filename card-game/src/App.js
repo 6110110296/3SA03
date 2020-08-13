4041987054e401d2725be6eae414d1e2803d1d05
PONGSATHON CHODCHOI (พงศธร ชดช้อย) <6110110296@email.psu.ac.th>
2020-08-13 21:34:37 +0700

    print array of character (Hello)

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'; 
 import './App.css'; 
 import CharacterCard from './CharacterCard';
+const word = "Hello";
 
 function App() {
   return ( 
     <div> 
-      <CharacterCard value="h"/>
-      <CharacterCard value="i"/>
+      {
+        Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+      }
     </div> 
   ); 
 } 
